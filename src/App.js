1bc2649164b12d8d0b18707e7daaddf3915d12ae
sahermostafa <sahermostafa>
2021-10-27 02:02:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CourseForm from './components/CourseForm'
+import CourseList from './components/CourseList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    courses : [
+      {name : "HTML"},
+      {name : "CSS"},
+      {name : "jQuery"},
+    ] ,
+    current : ''
+  }
+
+
+  // UpdateCourse
+  updateCourse = (e) => {
+    this.setState({
+      current: e.target.value
+    })
+  }
+
+  // addCourse
+  addCourse = (e) => {
+    e.preventDefault();
+    let current = this.state.current;
+    let courses = this.state.courses;
+    courses.push({name: current})
+    this.setState({
+      courses,
+      current: ''
+    })
+  }
+
+  // deleteCourse
+  deleteCourse = (index) => {
+    let courses = this.state.courses;
+    courses.splice(index , 1);
+    this.setState({
+      courses
+    })
+  }
+
+  // editCourse 
+  editCourse = (index,value) => {
+    let courses = this.state.courses;
+    let course = courses[index];
+    course['name'] = value;
+    this.setState({
+      courses
+    })
+  }
+  
+  render() {
+    const {courses} = this.state;
+    const courseList = courses.map( (course , index) => {
+      return <CourseList details={course} key={index} index={index} update={this.handleChange} deleteCourse={this.deleteCourse} editCourse={this.editCourse}/>
+    })
+    return (
+      <section className="App">
+        
+        <h2>Add Course</h2>
+        <CourseForm current={this.state.current} updateCourse={this.updateCourse} addCourse={this.addCourse}/> 
+        <ul>{courseList}</ul>
+
+      </section>
+    );
+  }
 }
 
 export default App;
